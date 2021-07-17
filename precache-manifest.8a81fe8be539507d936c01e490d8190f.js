677399a6e6a8ad8a0869069205414993175ef3e0
Code Sandbox <hello@codesandbox.io>
2021-07-17 15:47:57 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.8a81fe8be539507d936c01e490d8190f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68fbea9ecabc6adfbaf4a3c10ab9eb61",
+    "url": "https://kola9999.github.io/csb-5oxep/index.html"
+  },
+  {
+    "revision": "bf992df09eaa4441021f",
+    "url": "https://kola9999.github.io/csb-5oxep/static/css/2.8ab860fe.chunk.css"
+  },
+  {
+    "revision": "c90863100b00d4ff9c96",
+    "url": "https://kola9999.github.io/csb-5oxep/static/css/main.bbbbf122.chunk.css"
+  },
+  {
+    "revision": "bf992df09eaa4441021f",
+    "url": "https://kola9999.github.io/csb-5oxep/static/js/2.8a842652.chunk.js"
+  },
+  {
+    "revision": "731f815fa135396b018bfdb203439618",
+    "url": "https://kola9999.github.io/csb-5oxep/static/js/2.8a842652.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c90863100b00d4ff9c96",
+    "url": "https://kola9999.github.io/csb-5oxep/static/js/main.121b57ca.chunk.js"
+  },
+  {
+    "revision": "ec8afc81de5ccd0ac1f2",
+    "url": "https://kola9999.github.io/csb-5oxep/static/js/runtime-main.09d51597.js"
+  },
+  {
+    "revision": "813b6e7d5eec5840ff2929436c6a206c",
+    "url": "https://kola9999.github.io/csb-5oxep/static/media/404.813b6e7d.png"
+  }
+]);
